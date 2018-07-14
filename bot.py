287d4350e54f101dcb251f83bbf3af62f16f4d60
Insane7k <38253150+Insane7k@users.noreply.github.com>
2018-07-14 14:02:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,23 +20,25 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.content.startswith("r+8ball"):
-        await bot.send_message(message.channel, random.choice(["It is certain :8ball:",
-                                                               "Without a doubt :8ball:",
-                                                               "Yes, definitely :8ball:",
-                                                               "You may rely on it :8ball:",
-                                                               "As I see it, yes :8ball:",
-                                                               "Most likely :8ball:",
-                                                               "Outlook good :8ball:",
-                                                               "Yes :8ball:",
-                                                               "Signs point to yes :8ball:",
-                                                               "Ask again later :8ball:",
-                                                               "Better not tell you now :8ball:",    
-                                                               "Concentrate and ask again :8ball:",
-                                                               "Don't count on it :8ball:",
-                                                               "My reply is no :8ball:",
-                                                               "My sources say no :8ball:",
-                                                               "Outlook not so good :8ball:",
-                                                               "Very doubtful :8ball:"]))
+        await bot.send_message(message.channel, random.choice(['''
+It is certain :8ball:
+Without a doubt :8ball:
+Yes, definitely :8ball:
+  You may rely on it :8ball:
+As I see it, yes :8ball:
+  Most likely :8ball:
+Outlook good :8ball:
+Yes :8ball:",
+Signs point to yes :8ball:
+Ask again later :8ball:
+Better not tell you now :8ball:    
+Concentrate and ask again :8ball:
+Don't count on it :8ball:
+My reply is no :8ball:
+My sources say no :8ball:
+Outlook not so good :8ball:
+Very doubtful :8ball
+''']))
         
 @bot.command(pass_context = True)
 async def unmute(ctx, member: discord.Member):
