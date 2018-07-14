bf60a0a2de1b9ffc307706fd32129d79783d8dd3
Insane7k <38253150+Insane7k@users.noreply.github.com>
2018-07-14 13:24:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
+import random
+import os
+import pickle
 
 bot = commands.Bot(command_prefix='r+')
 
@@ -13,6 +16,32 @@ async def on_ready():
     print ("My Username currently is: " + bot.user.name)
     print ("my current ID is: " + bot.user.id)
     await bot.change_presence(game=discord.Game(name='v1.0.0 - r+bothelp'))
+    
+@bot.event
+async def on_message(message):
+    if message.author == client.user():
+        return
+    elif message.content.startswith("r+8ball"):
+        await bot.send_message(message.channel, random.choice([" "It is certain :8ball:",
+                                                               "It is decidedly so :8ball:",
+                                                               "Without a doubt :8ball:",
+                                                               "Yes, definitely :8ball:",
+                                                               "You may rely on it :8ball:",
+                                                               "As I see it, yes :8ball:",
+                                                               "Most likely :8ball:",
+                                                               "Outlook good :8ball:",
+                                                               "Yes :8ball:",
+                                                               "Signs point to yes :8ball:",
+                                                               "Reply hazy try again :8ball:",
+                                                               "Ask again later :8ball:",
+                                                               "Better not tell you now :8ball:",
+                                                               "Cannot predict now :8ball:",
+                                                               "Concentrate and ask again :8ball:",
+                                                               "Don't count on it :8ball:",
+                                                               "My reply is no :8ball:",
+                                                               "My sources say no :8ball:",
+                                                               "Outlook not so good :8ball:",
+                                                               "Very doubtful :8ball:"]))
         
 @bot.command(pass_context = True)
 async def unmute(ctx, member: discord.Member):
